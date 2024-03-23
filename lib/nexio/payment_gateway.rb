22ad47bc23fed6f450e087c43feeb3e883cd625c
Abdul Barek <barek2k2@gmail.com>
2024-03-23 12:11:50 -0600

    Refund option is exposed and written test cases

--- lib/nexio/payment_gateway.rb
+++ lib/nexio/payment_gateway.rb
@@ -142,6 +142,21 @@ module Nexio
       response_or_raise_error(response)
     end
 
+    # Amount is in USD
+    def self.refund(nexio_payment_id, amount)
+      url = URI("#{Nexio.configuration.api_server_url}/pay/v3/refund")
+      @request = Net::HTTP::Post.new(url)
+      http, request = configure_https_request(url, @request)
+      request.body = {
+        "id" => nexio_payment_id,
+        "data" => {
+          "currency" => "USD", "amount" => amount
+        },
+      }.to_json
+      response = http.request(request)
+      response_or_raise_error(response)
+    end
+
     # https configuration using base64 basic auth code
     def self.configure_https_request(url, request)
       http = Net::HTTP.new(url.host, url.port)
--- test/test_nexio.rb
+++ test/test_nexio.rb
@@ -8,6 +8,7 @@ class TestNexio < Minitest::Test
   def setup
     Nexio.configure do |config|
       config.api_key = "YmFyZWsyazJAZ21haWwuY29tOjExMTExMTExMTExMTExYUE="
+      config.environment = "test"
     end
   end
 
@@ -30,8 +31,8 @@ class TestNexio < Minitest::Test
             "expirationMonth" => "10",
             "expirationYear" => "#{Date.today.year + 10}",
             # follow this for generating encrypted card number https://jsfiddle.net/nexiopaydev/qkwautgb/30/
-            # original card number is 4000200011112222
-            "encryptedNumber" => "JQ2DIwFqQOCypsOE+3n0Mx6W6das1LrFAQVFR1lBD9KySCbVQXvJoweQ7R3wCv34oK6d8QlYQgsAWpmcROiwe4LowQI3pLfADmGRg4arowdaW8UBcR3gm26tT7KUdG13Y+0aiTKSleSJiRUSm3yU/VrNMe1tblYG+SsmtC8c3PEZkQxkJ216RYCzBkFRku2O7TRvx/GtxGd4VQItIF567VanRmZ8tIUaZGg9ZN6PKzUifRfCCt+2XGY7I1+Z7EOEAX1gQZT86+2vzcdk8MiZtMS4KYs+4kngSxR2EhyJa+3wRQBmkApRt03qCoWJEPIbNYxgwdjapy2oWeI/DrZu6A=="
+            # original card number is 5105105105105100
+            "encryptedNumber" => "NfveoPT0AyDMQtGqITRAjRnQmcbnlnMCg36UMSj4OFa8ZIvtz+3TML6hkEpFPXCk9/u5X/Qoj/9RIFfmdSmKZ83q/2tD5md76pjic7K4Gsz97w668rxiNFOuMn1eQWqReU2+6e8QvYbo4+3pXquXOdAuZyykwp+V2FvWmaE/Z/IX5K9MZuhJWX6ANts8FcwC98JTDPuD9hWRY1vyGs36XrvOMfrT3yLiZQrBd0Syib3F/PRPWZdJMQBQaFwI4gAM30vi5HlV3aB+DzsG31t52R9I1Ci0tH+pYtOpMZEXaPE8bsD8MwYzZcDUQ4V+7ntCOKeYrApD0pqViXSfcpnDfg=="
           },
           "data" => {
             "currency" => "USD"
@@ -47,6 +48,41 @@ class TestNexio < Minitest::Test
     end
     refute_nil @charge["authCode"]
     assert_equal 20.49, @charge["amount"].to_f
+  end
+
+  def test_refund
+    VCR.use_cassette('save_credit_card') do
+      @nexio_one_time_token = create_one_time_token
+      @card = Nexio::PaymentGateway.save_card(
+        {
+          "card" => {
+            "cardHolderName" => "Abdul Barek",
+            "expirationMonth" => "10",
+            "expirationYear" => "#{Date.today.year + 10}",
+            # follow this for generating encrypted card number https://jsfiddle.net/nexiopaydev/qkwautgb/30/
+            # original card number is 5105105105105100
+            # The amount is settled immediately for this card.
+            "encryptedNumber" => "NfveoPT0AyDMQtGqITRAjRnQmcbnlnMCg36UMSj4OFa8ZIvtz+3TML6hkEpFPXCk9/u5X/Qoj/9RIFfmdSmKZ83q/2tD5md76pjic7K4Gsz97w668rxiNFOuMn1eQWqReU2+6e8QvYbo4+3pXquXOdAuZyykwp+V2FvWmaE/Z/IX5K9MZuhJWX6ANts8FcwC98JTDPuD9hWRY1vyGs36XrvOMfrT3yLiZQrBd0Syib3F/PRPWZdJMQBQaFwI4gAM30vi5HlV3aB+DzsG31t52R9I1Ci0tH+pYtOpMZEXaPE8bsD8MwYzZcDUQ4V+7ntCOKeYrApD0pqViXSfcpnDfg=="
+          },
+          "data" => {
+            "currency" => "USD"
+          },
+          "shouldUpdateCard" => true,
+          "token" => @nexio_one_time_token
+        }
+      )
+    end
+
+    VCR.use_cassette('charge_credit_card') do
+      @charge = Nexio::PaymentGateway.charge(20.49,@card["token"]["token"])
+      @payment_id = @charge["id"]
+    end
+    VCR.use_cassette('refund') do
+      @refund = Nexio::PaymentGateway.refund(@payment_id,1.20)
+      assert_equal "refund",@refund["transactionType"]
+      assert_equal -1.20,@refund["amount"]
+      refute_nil @refund["authCode"]
+    end
   end
 
   private
